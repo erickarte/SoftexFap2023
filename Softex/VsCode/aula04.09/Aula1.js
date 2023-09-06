d64a47029b7495ec13bac66757c1d819cb4ae512
erickarte <erickarte@gmail.com>
2023-09-06 08:52:56 -0300

    Update Aula1.js

---
+++ Softex/VsCode/aula04.09/Aula1.js
@@ -0,0 +1,12 @@
+const valor = [];
+const valorquad = []
+
+for (let i = 0; i < 5; i++) {
+    valor.push(Number(prompt(`Informe o ${i}° número:`)));
+}
+console.log(valor);
+
+for (let i = 0; i < array.length; i++) {
+    valorquad.push(valor[i]**2);
+}
+console.log(valorquad);
