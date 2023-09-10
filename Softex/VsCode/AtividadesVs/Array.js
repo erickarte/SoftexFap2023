f9afe80444b522feeb06f293bd21049f69bcecde
erickarte <erickarte@gmail.com>
2023-09-10 20:47:49 -0300

    novas atividades

--- Softex/VsCode/AtividadesVs/Array.js
+++ Softex/VsCode/AtividadesVs/Array.js
@@ -1,12 +1,12 @@
 const valor = [];
 const valorquad = []
 
-for (let i = 0; i < 5; i++) {
+for (let i = 1; i <= 5; i++) {
     valor.push(Number(prompt(`Informe o ${i}° número:`)));
 }
 console.log(valor);
 
-for (let i = 0; i < valor.length; i++) {
+for (let i = 1; i <=valor.length; i++) {
     valorquad.push(valor[i]**2);
 }
 console.log(valorquad);
---
+++ Softex/VsCode/AtividadesVs/Exercício 01.03.01.js
@@ -0,0 +1,19 @@
+let dados = [
+    ["João", 25, "Azul"],
+    ["Maria", 30, "Verde"],
+    ["Pedro", 20, "Vermelho"],
+    ["Ana", 35, "Roxo"],
+    ["Lucas", 22, "Amarelo"],
+    ["Carla", 28, "Laranja"],
+    ["Mariana", 27, "Preto"],
+    ["Luiz", 32, "Branco"],
+    ["Julia", 29, "Rosa"],
+    ["Gustavo", 24, "Cinza"],
+];
+
+console.log(dados);
+
+dados[1][2] = "Margenta";
+dados[8][1] = 42;
+
+console.log(dados);
